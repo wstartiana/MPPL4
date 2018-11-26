d024b8685db16c37b80c8c4f1bd4d2eb271ed569
veliadhe <veliaaime@gmail.com>
2018-11-26 18:05:45 +0700

    peta

--- sekolahQ/resources/views/partials/script.blade.php
+++ sekolahQ/resources/views/partials/script.blade.php
@@ -35,3 +35,134 @@
 <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('dist/js/demo.js') }}"></script>
+<script type="text/javascript">  // This example adds a search box to a map, using the Google Place Autocomplete
+  // feature. People can enter geographical searches. The search box will return a
+  // pick list containing a mix of places and predicted search terms.
+
+  // This example requires the Places library. Include the libraries=places
+  // parameter when you first load the API. For example:
+  // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+  var citymap = {
+    ipb: {
+      center: {lat: -6.558118, lng: 106.726204},
+      population: 2000
+    },
+    botani_square: {
+      center: {lat: -6.601024, lng: 106.806876},
+      population: 3000
+    }
+  };
+
+  var citymap2 = {
+    kfc_bogor: {
+      center: {lat: -6.621576, lng: 106.817384},
+      population: 3400
+    },
+    yogya_bogor: {
+      center: {lat: -6.603593, lng: 106.799379},
+      population: 2000
+    }
+  };
+
+  function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 37.090240, lng: -95.712891},
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.HYBRID
+    });
+    // Construct the circle for each value in citymap.
+    // Note: We scale the area of the circle based on the population.
+    for (var city in citymap) {
+      // Add the circle for this city to the map.
+      var cityCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 0.5,
+        fillColor: '#FF0000',
+        fillOpacity: 0.5,
+        map: map,
+        center: citymap[city].center,
+        radius: Math.sqrt(citymap[city].population) * 100
+      });
+    }
+
+    for (var city in citymap2) {
+      // Add the circle for this city to the map.
+      var cityCircle = new google.maps.Circle({
+        strokeColor: '#0000FF',
+        strokeOpacity: 0.8,
+        strokeWeight: 0.5,
+        fillColor: '#0000FF',
+        fillOpacity: 0.5,
+        map: map,
+        center: citymap2[city].center,
+        radius: Math.sqrt(citymap2[city].population) * 100
+      });
+    }
+
+var input = document.getElementById('searchInput');
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+var autocomplete = new google.maps.places.Autocomplete(input);
+autocomplete.bindTo('bounds', map);
+
+var infowindow = new google.maps.InfoWindow();
+var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+});
+
+autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+    } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+    }
+    marker.setIcon(({
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+
+    //Location details
+    for (var i = 0; i < place.address_components.length; i++) {
+        if(place.address_components[i].types[0] == 'postal_code'){
+            document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+        }
+        if(place.address_components[i].types[0] == 'country'){
+            document.getElementById('country').innerHTML = place.address_components[i].long_name;
+        }
+    }
+    document.getElementById('location').innerHTML = place.formatted_address;
+    document.getElementById('lat').innerHTML = place.geometry.location.lat();
+    document.getElementById('lon').innerHTML = place.geometry.location.lng();
+});
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDJfQgQL9MaM83aHUs5vxwi6O6JeVoU63Y&libraries=places&callback=initAutocomplete" async defer></script>
--- sekolahQ/resources/views/peta.blade.php
+++ sekolahQ/resources/views/peta.blade.php
@@ -1,6 +1,6 @@
 @extends('templates.master')
 
 @section('content')
-
+<div id="map" style="width: 1500px; height: 500px"></div></align> 
 
 @endsection
